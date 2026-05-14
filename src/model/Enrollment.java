f4e4a75f71e0c00c43147d6b1388b05b1e0c45eb
JalexSM <jsamayoam9@miumg.edu.gt>
2026-05-13 20:40:07 -0600

    feat: implementacion de arrays en clases necesarias

--- src/model/Enrollment.java
+++ src/model/Enrollment.java
@@ -1,42 +1,53 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Enrollment {
-	
+
 	private Student student;
 	private Course course;
+	private ArrayList<Double> grades;
+
 	public Enrollment(Student student, Course course) {
 		super();
 		this.student = student;
 		this.course = course;
+		this.grades = new ArrayList<>();
 	}
+
 	public Student getStudent() {
 		return student;
 	}
+
 	public void setStudent(Student student) {
 		this.student = student;
 	}
+
 	public Course getCourse() {
 		return course;
 	}
+
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+
+	public ArrayList<Double> getGrades() {
+		return grades;
+	}
+
+	public void addGrade(double grade) {
+		grades.add(grade);
+
+	}
+
 	@Override
 	public String toString() {
-	    return """
-	           Enrollment
-	           -------------------------
-	           Student : %s %s
-	           Course  : %s
-	           """.formatted(
-	                   student.getFirstName(),
-	                   student.getLastName(),
-	                   course.getCourseName()
-	           );
+		return """
+				Enrollment
+				-------------------------
+				Student : %s %s
+				Course  : %s
+				""".formatted(student.getFirstName(), student.getLastName(), course.getCourseName());
 	}
-	
-	
-	
-	
 
 }
